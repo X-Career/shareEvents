80609410dbdba16cf1ceba3fd4eb3afae8f77870
VŨ NGUYỄN MINH TIẾN <petrustien@192.168.2.16>
2023-10-26 22:39:54 +0700

    update seat

--- backend/src/controllers/seat.js
+++ backend/src/controllers/seat.js
@@ -55,12 +55,68 @@ const getAllSeats = async (req, res) => {
 };
 
 const getSeatById = async (res, req) => {
+    try {
+        const idSeat = req.params.id;
+        const data = await seatModel.findById(idSeat).populate("events")
+        if (!data) {
+            return res.status(404).json({ message: "Seat is not found" });
+        }
+        return res.status(200).json({
+            message: "Seat is successfully",
+            datas: data,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }    
 };
 
 const updateSeat = async (res, req) => {
+    try {
+        const { error } = seatValidator.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                message: error.details[0].message || "Please re-check your data!",
+            });
+        }
+
+        const idSeat = req.params.id;
+        const data = await  seatModel.findByIdAndUpdate(idSeat, req.body, { new: true })
+        if (!data) {
+            return res.status(404).json({
+                message: "Updating Seat is not successful",
+            });
+        }
+        return res.status(200).json({
+            message: "Updating Seat is successful",
+            data: data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
 };
 
 const deleteSeat = async (res, req) => {
+    try {
+        const idSeat = req.params.id;
+        const data = await categoryModel.findByIdAndDelete(idSeat)
+
+        if (!data) {
+            return res.status(404).json({
+                message: "Deleting Seat is not successful",
+            });
+        }
+        return res.status(200).json({
+            message: "Deleting Seat is successful",
+            data
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
 };
 
 module.exports = {
