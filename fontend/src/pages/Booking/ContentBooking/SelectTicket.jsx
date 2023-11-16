b1e7d42e76f15286bbcec61db285c99a496cc8f5
TriNguyen38 <caotri3894@gmail.com>
2023-11-16 23:59:22 +0700

    update Booking-page

--- fontend/src/pages/Booking/ContentBooking/SelectTicket.jsx
+++ fontend/src/pages/Booking/ContentBooking/SelectTicket.jsx
@@ -4,90 +4,119 @@ import "./SelectTicket.css";
 const SelectTicket = () => {
   const [isCheck, setIsCheck] = useState(false);
   const [circleInfo, setCircleInfo] = useState(null);
-  const [ticketClick, setTicketClick] = useState([])
+  const [ticketClick, setTicketClick] = useState([]);
+
   const handleMouseEnter = (stage) => {
     setCircleInfo(stage);
   };
 
-
   const handleMouseLeave = () => {
     setCircleInfo(null);
   };
+
   const handleClick = () => {
-    setTicketClick([...ticketClick,circleInfo]);
-  }
+    const isSelected = ticketClick.find(
+      (ticket) => ticket.id === circleInfo.id
+    );
+    console.log(isSelected);
+    if (isSelected) {
+      const updatedTickets = ticketClick.filter(
+        (ticket) => ticket.id !== circleInfo.id
+      );
+      setTicketClick(updatedTickets);
+      console.log(ticketClick);
+    } else {
+      const selectedTicket = fakeSeat.find(
+        (ticket) => ticket.id === circleInfo.id
+      );
+      setTicketClick([...ticketClick, selectedTicket]);
+    }
+  };
   useEffect(() => {
-    console.log(ticketClick);
+    console.log("ticketClick", ticketClick);
   }, [ticketClick]);
-  
-
-
   const handleCheck = () => {
     setIsCheck(!isCheck);
     console.log(isCheck);
   };
   const fakeSeat = [
     {
+      id: 1,
       type: "Standard",
       price: 205,
     },
     {
+      id: 2,
       type: "Standard",
       price: 206,
     },
     {
+      id: 3,
       type: "Vip",
       price: 350,
     },
     {
+      id: 4,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 5,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 6,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 7,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 8,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 9,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 10,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 11,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 12,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 13,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 14,
       type: "VVip",
       price: 500,
     },
     {
+      id: 15,
       type: "V-Vip",
       price: 500,
     },
     {
+      id: 16,
       type: "V-Vip",
       price: 500,
     },
@@ -129,95 +158,109 @@ const SelectTicket = () => {
         <div className="seatMap">
           <span className="title-seatMap">Stage/Sân khấu</span>
           {/* <div className="seatMapComponent"> */}
-            <div className="seatMapComponent-VVip">
-              {fakeSeat
-                .filter((stage) => stage.type === "V-Vip")
-                .map((stage,index) => (
-                  <div
-                    className={`model-${stage.type}`}
-                    // key= {`${stage.type}-${stage.price}-${index}`}
-                    key = {index}
-                    onMouseEnter={() => handleMouseEnter(stage)}
-                    onMouseLeave={handleMouseLeave}
-                    onClick = {handleClick}
-                  >
-                    <div className="numberSeat">
-                      {stage.index}
-                      {circleInfo && (
-                        <div className="circle-info">
-                          <span>{stage.price} VND</span>
-                          <span>{stage.type}</span>
-                        </div>
-                      )}
-                    </div>
+          <div className="seatMapComponent-VVip">
+            {fakeSeat
+              .filter((stage) => stage.type === "V-Vip")
+              .map((stage, index) => (
+                <div
+                  className={`model-${stage.type} ${
+                    ticketClick.find((ticket) => ticket.id === stage.id)
+                      ? "selected"
+                      : ""
+                  }`}
+                  // className={`model-${stage.type}`}
+                  // key= {`${stage.type}-${stage.price}-${index}`}
+                  key={index}
+                  onMouseEnter={() => handleMouseEnter(stage)}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={handleClick}
+                >
+                  <div className="numberSeat">
+                    {stage.index}
+                    {circleInfo && (
+                      <div className="circle-info">
+                        <span>{stage.price} VND</span>
+                        <span>{stage.type}</span>
+                      </div>
+                    )}
                   </div>
-                ))}
-            </div>
-            <div className="seatMapComponent-Vip">
-              {fakeSeat
-                .filter((stage) => stage.type === "Vip")
-                .map((stage,index) => (
-                  <div
-                    className={`model-${stage.type}`}
-                    // key={`${stage.type}-${stage.price}-${index}`}
-                    key= {index}
-                    onMouseEnter={() => handleMouseEnter(stage)}
-                    onMouseLeave={handleMouseLeave}
-                    onClick = {handleClick}
-
-                  >
-                    <div className="numberSeat">
-                      {stage.index}
-                      {circleInfo && (
-                        <div className="circle-info">
-                          <span>{stage.price}</span>
-                          <span>{stage.type}</span>
-                        </div>
-                      )}
-                    </div>
+                </div>
+              ))}
+          </div>
+          <div className="seatMapComponent-Vip">
+            {fakeSeat
+              .filter((stage) => stage.type === "Vip")
+              .map((stage, index) => (
+                <div
+                  className={`model-${stage.type} ${
+                    ticketClick.find((ticket) => ticket.id === stage.id)
+                      ? "selected"
+                      : ""
+                  }`}
+                  // className={`model-${stage.type}`}
+                  // key={`${stage.type}-${stage.price}-${index}`}
+                  key={index}
+                  onMouseEnter={() => handleMouseEnter(stage)}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={handleClick}
+                >
+                  <div className="numberSeat">
+                    {stage.index}
+                    {circleInfo && (
+                      <div className="circle-info">
+                        <span>{stage.price}</span>
+                        <span>{stage.type}</span>
+                      </div>
+                    )}
                   </div>
-                ))}
-            </div>
-            <div className="seatMapComponent-Standard">
-              {fakeSeat
-                .filter((stage) => stage.type === "Standard")
-                .map((stage,index) => (
-                  <div
-                    className={`model-${stage.type}`}
-                    // key={`${stage.type}-${stage.price}-${index}`}
-                    key= {index}
-                    onMouseEnter={() => handleMouseEnter(stage)}
-                    onMouseLeave={handleMouseLeave}
-                    onClick = {handleClick}
-
-                  >
-                    <div className="numberSeat">
-                      {stage.index}
-                      {circleInfo && (
-                        <div className="circle-info">
-                          <span>{stage.price}</span>
-                          <span>{stage.type}</span>
-                        </div>
-                      )}
-                    </div>
+                </div>
+              ))}
+          </div>
+          <div className="seatMapComponent-Standard">
+            {fakeSeat
+              .filter((stage) => stage.type === "Standard")
+              .map((stage, index, ticket) => (
+                <div
+                  className={`model-${stage.type} ${
+                    ticketClick.find((ticket) => ticket.id === stage.id)
+                      ? "selected"
+                      : ""
+                  }`}
+                  // className={`model-${stage.type}`}
+                  // key={`${stage.type}-${stage.price}-${index}`}
+                  key={index}
+                  onMouseEnter={() => handleMouseEnter(stage)}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={handleClick}
+                >
+                  <div className="numberSeat">
+                    {stage.index}
+                    {circleInfo && (
+                      <div className="circle-info">
+                        <span>{stage.price}</span>
+                        <span>{stage.type}</span>
+                      </div>
+                    )}
                   </div>
-                ))}
-            </div>
+                </div>
+              ))}
+          </div>
           {/* </div> */}
         </div>
       </div>
       <div className="info-payment">
         <span className="infoTicket">Thông tin đặt vé</span>
         <hr />
-        {ticketClick && (
-          ticketClick.map((ticket,index) =>(
-          <div className="ticketInfo" key={index}>
-            <span>{ticket.type}</span>
-            <span>{ticket.price}</span>
-          </div>
-        ))
+        {ticketClick && ticketClick.length > 0 ? (
+          ticketClick.map((ticket, index) => (
+            <div className="ticketInfo" key={index}>
+              <span>{ticket.type}</span>
+              <span>{ticket.price}</span>
+            </div>
+          ))
+        ) : (
+          <span className="picketTicket">Vui lòng chọn vé</span>
         )}
-        <span className="picketTicket">Vui lòng chọn vé</span>
         <div className="total-price">
           <span>Tổng cộng</span>
           <span>0 VND</span>
